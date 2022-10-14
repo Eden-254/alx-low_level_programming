4c5e05cd9edc9c8110012f8f40e6e2c01f25370a
Eden-254 <otandoeden@gmail.com>
2022-10-14 10:00:12 +0000

    forgot compilation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main -Entry point 
  *
- * Return: always 0
+ * Return:always 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
