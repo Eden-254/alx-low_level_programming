6b12e77aa9f5c84b74a0c4bfddc1900221d0ca7c
Eden-254 <otandoeden@gmail.com>
2022-10-18 09:02:50 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Check the code
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+}
