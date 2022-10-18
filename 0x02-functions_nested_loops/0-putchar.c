2828a9f6e8e43b52f746c22e3ec80faf5bf42bcc
Eden-254 <otandoeden@gmail.com>
2022-10-18 04:46:01 -0700

    hope this works

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,6 +8,14 @@
  */
 int main(void)
 {
-	printf("_putchar \n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
