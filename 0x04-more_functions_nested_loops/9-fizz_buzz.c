d362352f254304280dcd16238b41e52cd1c41cc7
Eden-254 <otandoeden@gmail.com>
2022-10-21 12:31:45 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints a Fizz Buzz program
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
+
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
