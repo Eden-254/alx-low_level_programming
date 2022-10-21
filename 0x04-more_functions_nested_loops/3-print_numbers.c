4201104f8a0ae4c6191a21457cd6580c76e28dd4
Eden-254 <otandoeden@gmail.com>
2022-10-21 07:11:07 -0700

    checker4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
  * print_numbers - Print the numbers since 0 up to 9
  *
- * Return: The numbers since 0 up to 9
- *
+ * Return:Always 0
  */
 void print_numbers(void)
 {
