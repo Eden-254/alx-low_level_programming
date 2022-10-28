00ac994e8caac44163162c0cf9c46d611572655e
Eden-254 <otandoeden@gmail.com>
2022-10-28 06:31:35 -0700

    fixed third task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,10 +5,11 @@
  * @s2: second string
  *
  * Return: value of string
+ *
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
-	int = 0;
+	int i = 0;
 
 	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
 	{
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reverse_array - reverses an array
+ *
+ * @a: array to reverse
+ * @n: size of array
+ *
+ * Return: empty
+ */
+void reverse_array(int *a, int n)
+{
+
